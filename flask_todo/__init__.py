75ba1da415406240f08709a93ac7f1b4414b0a90
Jonathan Marciano <bobthemoon@gmail.com>
2019-03-26 18:12:43 -0400

    - I forgot to commit - -_-

--- flask_todo/__init__.py
+++ flask_todo/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request, make_response, render_template
 
 
@@ -13,51 +15,35 @@ def create_app(test_config=None):
     else:
         app.config.from_mapping(test_config)
 
+    from . import db
+    db.init_app(app)
+
     @app.route('/')
     def index():
         return render_template('index.html')
 
-    @app.route('/hello')
-    def hello():
-        name = request.args.get('name', 'World')
-        return render_template('hello.html', name=name)
-
-    @app.route('/number/<int:num>')
-    def number_route(num):
-        return f"Number: {num}"
-
-    @app.route('/calculate', methods=['GET', 'POST'])
-    def calculate():
-        if request.method == 'GET':
-            return render_template('calculate.html', action="Add")
-        elif request.method == 'POST':
-            x = float(request.form['x'])
-            y = float(request.form['y'])
-            action = request.form['action']
-
-            if action == "Add":
-                result = x + y
-            elif action == "Subtract":
-                result = x - y
-            elif action == "Multiply":
-                result = x * y
-            elif action == "Divide":
-                result = x / y
-
-        return render_template('calculate.html', result=result, x=x, y=y)
+    @app.route('/new', methods=('GET', 'POST'))
+    def ne():
+        if request.method == 'POST':
+            item = request.form['item']
+            error = None
+            db = get_db()
+            db.execute(
+                'INSERT INTO list (item, done)'
+                ' VALUES (?, FALSE)',
+                (item, done)
+            )
+            db.commit()
+            return redirect('index.html')
+        return render_template('new.html')
 
 
-    method_route_allows = ['GET', 'POST', 'PATCH', 'PUT', 'DELETE']
+    @app.route('/delete')
+    def dal():
+        return render_template('delete.html')
 
-    @app.route('/method', methods=method_route_allows)
-    def method():
-        return render_template('method.html', allowed=method_route_allows, method=request.method, action=action)
 
-    @app.route('/status')
-    def status():
-        yes = request.args.get('c', 200)
-        response = make_response(f"status: {code}", yes)
 
-        return response
+
 
     return app
